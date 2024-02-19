65e2d38a3aec45fbba62683b4e53434fcee95a6f
root <root@DV23-1>
2024-02-19 21:54:03 +0700

    feat: internet identity

---
+++ src/HireVerse_frontend/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { HireVerse_backend, createActor } from 'declarations/HireVerse_backend';
+import { AuthClient } from "@dfinity/auth-client";
+import { HttpAgent } from '@dfinity/agent';
+
+function App() {
+  const [greeting, setGreeting] = useState('null');
+
+  let actor = HireVerse_backend;
+
+  async function handleGreet(event) {
+    event.preventDefault();
+
+    const actorGreet = await actor.greet();
+
+    setGreeting(actorGreet);
+
+    return false;
+  }
+
+  const login = async (event) => {
+    event.preventDefault();
+    let authClient = await AuthClient.create();
+
+    // start the login process and wait for it to finish
+    await new Promise((resolve) => {
+        authClient.login({
+            identityProvider: 'http://bnz7o-iuaaa-aaaaa-qaaaa-cai.localhost:4943',
+            onSuccess: resolve,
+        });
+    });
+
+    const identity = authClient.getIdentity();
+    
+    const agent = new HttpAgent({identity});
+
+    actor = createActor(process.env.CANISTER_ID_HIREVERSE_BACKEND, {
+        agent,
+    });
+
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form onSubmit={login}>
+        <button type='submit'>Login!</button>
+      </form>
+
+      <form onSubmit={handleGreet}>
+        <button type="submit">Greet</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
