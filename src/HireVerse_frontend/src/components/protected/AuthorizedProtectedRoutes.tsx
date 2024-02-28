05db756aadaab5ba87d59b1c16d290941a3b44c2
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-02-28 13:27:20 +0700

    feat; solve merge

--- src/HireVerse_frontend/src/components/protected/AuthorizedProtectedRoutes.tsx
+++ src/HireVerse_frontend/src/components/protected/AuthorizedProtectedRoutes.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import useAuth, {AuthState} from "../../hooks/useAuth";
 import {Outlet, useNavigate} from "react-router-dom";
 import {useEffect} from "react";
@@ -7,15 +6,6 @@ import LoadingPagePlaceholder from "./LoadingPagePlaceholder";
 
 export default function AuthorizedProtectedRoutes({children}: { children?: React.ReactNode }) {
     const {authState} = useAuth();
-=======
-import useAuth, { AuthState } from "../../hooks/useAuth";
-import { Outlet, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-
-
-export default function AuthorizedProtectedRoutes({children} : {children?: React.ReactNode}) {
-    const { authState } = useAuth();
->>>>>>> 3c99d1d9b145ae96278fa1092f28957ef3557148
 
     const navigate = useNavigate();
 
@@ -27,7 +17,6 @@ export default function AuthorizedProtectedRoutes({children} : {children?: React
             console.log("Unregistered");
             navigate("/complete-registration");
         }
-<<<<<<< HEAD
         console.log("Protection for: ", authState);
     }, [authState]);
 
@@ -37,11 +26,5 @@ export default function AuthorizedProtectedRoutes({children} : {children?: React
         ) : (
             <LoadingPagePlaceholder />
         )
-=======
-    }, [authState]);
-
-    return (
-        children ?? <Outlet />
->>>>>>> 3c99d1d9b145ae96278fa1092f28957ef3557148
     );
 }
