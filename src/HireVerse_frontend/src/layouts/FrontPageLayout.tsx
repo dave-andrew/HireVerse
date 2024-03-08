5e7725d62bb30a3b9fa6591ba0a638448a12f71c
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-03-08 21:43:59 +0700

    Feat: Complete Registration UI

--- src/HireVerse_frontend/src/layouts/FrontPageLayout.tsx
+++ src/HireVerse_frontend/src/layouts/FrontPageLayout.tsx
@@ -32,7 +32,7 @@ export default function FrontPageLayout({children, className}: Props) {
     return (
         <>
             <ToastContainer/>
-            <div className="bg-signature-gray h-fit flex w-full snap-y flex-col">
+            <div className="bg-signature-gray h-fit flex w-full flex-col">
                 <Navbar/>
                 <div className={`mt-16 w-full rounded-xl bg-white ${className}`}>{children}</div>
             </div>
